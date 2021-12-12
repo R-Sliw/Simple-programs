4cb4b84c4abfaa0446434a00b4f221a005fbb6f4
R-Sliw <robert.sliwinski90@gmail.com>
2021-12-12 17:52:59 +0100

    first program added

---
+++ palindrome-checker.js
@@ -0,0 +1,14 @@
+function palindrome(str) {
+  let bigLetters = str
+    .toUpperCase()
+    .replace(/[^a-zA-Z1-9]/g, "");
+
+  let reverseWord = bigLetters
+    .split("")
+    .reverse()
+    .join("");
+
+return (bigLetters === reverseWord) ? true : false;
+
+}
+palindrome("race car");
